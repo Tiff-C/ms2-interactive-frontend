31fbb4c3854b0a4e5c65555f67f411bcd07bcb7d
Tiff-C <deguntiffany@gmail.com>
2021-08-18 15:10:22 +0000

    began writing js in script.js began adding onLoad function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log('hotels');
+document.addEventListener('DOMContentLoaded', function() {
+  let hotelLists = $('.top10-list-item');
+  let infoBox = $('.info-details').html()
+
+  for (let hotelList of hotelLists) {
+    for (let hotel of hotels) {
+      $(hotelList).click(function() {
+        infoBox = 
+        `<h5>${hotel.name}</h5>
+        <p>${hotel.info}</p>
+        <button><a src="${hotel.url}">Go to hotel website</a></button>`;
+      }); 
+    }
+  };
+}
+)
+
