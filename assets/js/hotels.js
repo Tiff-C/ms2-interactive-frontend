d5bdd375b6d72a483f38bb1a5930acbe2d11915a
Tiff-C <deguntiffany@gmail.com>
2021-08-18 13:31:55 +0000

    added london hotels to hotels.js added the script to accomodation.html and tested by logging hotels to console.

---
+++ assets/js/hotels.js
@@ -0,0 +1,82 @@
+let hotels = [
+  {
+    "city": "london",
+    "name": "The Resident Covent Garden",
+    "address": "51 Bedford Street",
+    "postcode": "WC2R 0PZ",
+    "url": "https://www.residenthotels.com/the-resident-covent-garden/",
+    "info": "The Resident Covent Garden is located overlooking the iconic Strand, in the heart of London's Covent Garden, within a 5 minute walk of both Covent Garden and Charing Cross stations. Just moments from the area's picturesque Italianate Piazza, it is your perfect base from which to discover one of London's most enchanting neighbourhoods and the wider city beyond."
+  },
+  {
+    "city": "london",
+    "name": "Hotel 41",
+    "address": "41 Buckingham Palace Road",
+    "postcode": "SW1W 0PS",
+    "url": "https://41hotel.com/",
+    "info": "Our residents-only hotel, with a prestigious Forbes Five-Star rating, has been recognised as the number one hotel in the UK for more than a decade.\nArrive at our private entrance overlooking the Royal Mews of Buckingham Palace, imbued with the subtle fragrance of Hotel 41’s own signature scent, and be welcomed with a personal check-in and a glass from our beautiful Champagne trolley. In your bedroom or suite, discover a selection of homemade treats created by our Executive Chef, along with a supremely comfortable, handcrafted Savoir bed. An unforgettable experience begins..."
+  },
+  {
+    "city": "london",
+    "name": "Great Scotland Yard",
+    "address": "3-5 Great Scotland Yard",
+    "postcode": "SW1A 2HN",
+    "url": "https://www.hyatt.com/en-US/hotel/england-united-kingdom/great-scotland-yard/lhrub",
+    "info": "Five star luxury hotel, located a stone’s throw away from Covent Garden. Great Scotland Yard is one of London’s most historic and cherished buildings that holds more hidden stories than any other kind in London. Where tradition and history meets modern luxury. One of a Kind. Stay in an 1820’s Grade II listed building with Edwardian & Victorian architecture, featuring 152 guest rooms including eight Suites and one Standalone Townhouse. Enjoy a unique and quintessentially quirky twist on a British classic, where no two rooms are the same—with stunning Mary Poppins views of London."
+  },
+  {
+    "city": "london",
+    "name": "The Hari",
+    "address": "20 Chesham Place",
+    "postcode": "SW1X 8HQ",
+    "url": "https://www.thehari.com/london/",
+    "info": "Welcome to The Hari, a stylish retreat in the heart of Belgravia, and your new home away from home in London. Designed for those seeking comfort and understated elegance, The Hari is a sanctuary for modern travellers and connoisseurs of luxury.\nBordering the areas of Chelsea, Kensington and Knightsbridge, Harrods is just moments away from The Hari’s Belgravia base. London’s leading attractions can also be found a short walk from the hotel, including Hyde Park, Buckingham Palace, The Royal Albert Hall and more."
+  },
+  {
+    "city": "london",
+    "name": "The Egerton House Hotel",
+    "address": "17 -19 Egerton Terrace",
+    "postcode": "SW3 2BX",
+    "url": "https://egertonhousehotel.com/",
+    "info": "Step through an arch of fragrant blooms to discover this enchanting boutique townhouse in the very heart of Knightsbridge, in the Royal Borough of Kensington and Chelsea. With a unique character and beautiful traditional style, enjoy a delicious breakfast, named the best in the UK, experience our whimsical \"Hatter's\" Afternoon Tea by the fire or sip on one of London’s most famous martinis."
+  },
+  {
+    "city": "london",
+    "name": "The Milestone Hotel & Residencies",
+    "address": "1 Kensington Court",
+    "postcode": "W8 5DL",
+    "url": "https://milestonehotel.com",
+    "info": "Step into our Grade-II listed Victorian building and discover old world charm, beautiful rich furnishings, exquisite original arts and outstanding cuisine. Instantly recognisable for our striking red-and-white brick façade, The Milestone is a true London landmark. Just a short stroll from the legendary Royal Albert Hall, as well as Notting Hill and Knightsbridge, our leafy surroundings are perfect for a beautiful picnic in the park."
+  },
+  {
+    "city": "london",
+    "name": "The Montcalm London Marbel Arch",
+    "address": "2 Wallenberg Place",
+    "postcode": "W1H 7TN",
+    "url": "https://montcalm.co.uk/",
+    "info": "Sitting grandly at the top of Park Lane, within close proximity to London's most exclusive addresses including Mayfair, Oxford and Regent Streets, Marylebone, Edgware Road and Hyde Park is The Montcalm London Marble Arch.\nAt The Montcalm London Marble Arch, the magnificent décor and the contemporary design of the lobby welcomes you to an experience to remember. The hotel features 153 bedrooms including 14 lavish suites. Our rooms are tastefully designed with an emphasis on comfort and style. Chic and contemporary, the bedrooms come complete with state of the art amenities including complimentary broadband and Wi-Fi, LCD TV with international and satellite channels, pillow menu and in-room aroma choices for a memorable stay."
+  },
+  {
+    "city": "london",
+    "name": "Royal Lancaster London",
+    "address": "Lancaster Terrace",
+    "postcode": "W2 2TY",
+    "url": "https://www.royallancaster.com",
+    "info": "Royal Lancaster London is Hyde Park’s commanding mid-century architectural icon born and built in the swinging 60s. Our happy history has seen us hip, handsome and rolling with the times in our unrivalled London location. Today, here in the 21st century we’re proud of the knowledge and expertise we’ve gained over half a century as one of London’s leading hotels.\nWe enjoy the liberty of being independent and the opportunity it gives us to express our unique and genuine personality. We take pride in having a conscientious, highly professional and happy team dedicated to delivering outstanding service and ensuring that every guest experience is delightfully memorable. Whether it’s the bees in the hives on our roof, the busy staff in our corridors and kitchens, or every guest that we welcome, ‘we always care’."
+  },
+  {
+    "city": "london",
+    "name": "The Stafford London",
+    "address": "16-18 St Jame's Place",
+    "postcode": "SW1A 1NJ",
+    "url": "https://thestaffordlondon.com/",
+    "info": "Located in the heart of historic St James’s, minutes from royal parks and palaces, The Stafford London offers world-class accommodation at one of London’s most prestigious addresses. Boasting three unique buildings, each with their own style and character, guests experience timeless luxury, combined with contemporary sophistication and impeccable service."
+  },
+  {
+    "city": "london",
+    "name": "One Aldwych",
+    "address": "One Aldwych",
+    "postcode": "WC2B 4BZ",
+    "url": "https://www.onealdwych.com/",
+    "info": "An address in London’s beating heart; One Aldwych is located in Covent Garden at the centre\nof culture and theatre. Our unique neighbourhood, with its elegant piazza and cobbled alleyways, is a foodie destination, a theatre lovers paradise and the gateway to the very best of London. \nA place for people who don’t do generic, One Aldwych is one of London’s finest, independent hotels."
+  }
+]
