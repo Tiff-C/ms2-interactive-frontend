4b76e09b82272e85f844f7857e7075b041dcf644
Tiff-C <deguntiffany@gmail.com>
2021-08-18 15:30:34 +0000

    Moved hotel of hotels loop out of onLoad function replaced it with an if else statement that calls displayHotel() and passes the correct index value for the londonHotels array for the corresponding 'button'

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
   let hotelLists = $('.top10-list-item');
-  let infoBox = $('.info-details').html()
 
   for (let hotelList of hotelLists) {
-    for (let hotel of hotels) {
+    $(this).click(function() {
+      if (hotelList.html() === 'Hotel 1') {
+        displayHotel(0);
+      } else if (hotelList.html() === 'Hotel 2') {
+        displayHotel(1);
+      } else if (hotelList.html() === 'Hotel 3') {
+        displayHotel(2);
+      } else if (hotelList.html() === 'Hotel 4') {
+        displayHotel(3);
+      } else if (hotelList.html() === 'Hotel 5') {
+        displayHotel(4);
+      } else if (hotelList.html() === 'Hotel 6') {
+        displayHotel(5);
+      } else if (hotelList.html() === 'Hotel 7') {
+        displayHotel(6);
+      } else if (hotelList.html() === 'Hotel 8') {
+        displayHotel(7);
+      } else if (hotelList.html() === 'Hotel 9') {
+        displayHotel(8);
+      } else if (hotelList.html() === 'Hotel 10') {
+        displayHotel(9);
+      }
+    })
+  };
+})
+
+
+
+
+/*
+space for code that will go into another function:
+
+let infoBox = $('.info-details').html()
+
+for (let hotel of hotels) {
       $(hotelList).click(function() {
         infoBox = 
         `<h5>${hotel.name}</h5>
@@ -11,7 +44,4 @@ document.addEventListener('DOMContentLoaded', function() {
         <button><a src="${hotel.url}">Go to hotel website</a></button>`;
       }); 
     }
-  };
-}
-)
-
+*/
